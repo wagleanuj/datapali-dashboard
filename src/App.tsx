3a312642b9df2d581a42b2aeaed4d46ece8a6566
Anuj <anujwagle@gmail.com>
2019-08-22 00:40:20 -0230

    form started

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { DPNavbar } from './ui/Navbar';
+import { DPFormItem } from './components/DPFormItem';
+import { Row } from 'reactstrap';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className={"wrapper"}>
+        <DPNavbar></DPNavbar>
+        <div className="main-panel">
+          <div className="content">
+            <DPFormItem></DPFormItem>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const hist = createBrowserHistory();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<Router history={hist}>
+    <Switch>
+      <Route path="/admin" render={props => <App {...props} />} />
+      <Redirect from="/" to="/admin/dashboard" />
+    </Switch>
+  </Router>, document.getElementById('root'));
+
+
 serviceWorker.unregister();
