d0b363ec7d71516c57ea9a71891350936bdb6ecf
Anuj Wagle <anujwagle@gmail.com>
2019-09-04 00:52:54 -0230

    can delete section and question on first section

--- src/components/SurveyForm.tsx
+++ src/components/SurveyForm.tsx
@@ -61,6 +61,13 @@ export class QuestionSection {
         this.content.push(content);
         return this;
     }
+
+    deleteContent(contentId: string) {
+        let found = this.content.findIndex(item => item.id === contentId);
+        if (found > -1) {
+            this.content.splice(found, 1);
+        }
+    }
 }
 
 interface SurveyFormState {
@@ -83,17 +90,25 @@ export class SurveyForm_ extends React.Component<SurveyFormProps, SurveyFormStat
     static defaultProps = {
         form: new QuestionSection().setContent([testQuestion, testQuestion2, testQuestion3, new QuestionSection().setContent([testQuestion4, testQuestion5]).setName("true things")]),
     }
+    public static questionOrSectionFromPath(path: number[], root: (QuestionSection | QAQuestion)[]): QuestionSection | QAQuestion {
+        let el = root[path[0]];
+        if (path.length === 1) {
+            return el;
+        } else {
+            return SurveyForm_.questionOrSectionFromPath(path.slice(1), el.content);
+        }
+    }
     constructor(props: SurveyFormProps) {
         super(props);
         this.state = {
             form: this.props.form,
             activeSection: this.props.form,
             treeNodes: this.generateITNodeTree(this.props.form),
-            activeSectionPath:[0]
+            activeSectionPath: [0]
         }
     }
 
-    getNodeFromQuestionOrSection(item: QuestionSection | QAQuestion, sectionNumber: string)  {
+    getNodeFromQuestionOrSection(item: QuestionSection | QAQuestion, sectionNumber: string) {
         let def: ITreeNode = {
             id: item.id,
             icon: undefined,
@@ -126,12 +141,35 @@ export class SurveyForm_ extends React.Component<SurveyFormProps, SurveyFormStat
 
     addSectionToNodeTree(_nodePath: number[], section: QuestionSection) {
         let node = Tree.nodeFromPath(_nodePath, this.state.treeNodes);
-        if(node.childNodes){
-            node.childNodes.push(this.getNodeFromQuestionOrSection(section,  node.childNodes.length.toString()))
+        if (node.childNodes) {
+            let readablePath = getReadablePath(this.state.activeSectionPath);
+            if (readablePath) readablePath += ".";
+            node.childNodes.push(this.getNodeFromQuestionOrSection(section, readablePath + (node.childNodes.length + 1).toString()))
         }
-       
-        console.log(node);
-        this.setState(this.state);
+
+        return this.state;
+    }
+
+
+    deleteNodeFromNodeTree(_nodePath: number[], id: string) {
+        let node = Tree.nodeFromPath(_nodePath, this.state.treeNodes);
+        if (node.childNodes) {
+            let foundIndex = node.childNodes.findIndex(item => item.id === id);
+            if (foundIndex > -1) {
+                node.childNodes.splice(foundIndex, 1)
+            }
+        }
+        return this.state;
+    }
+
+    addQuestionToNodeTree(_nodePath: number[], section: QAQuestion) {
+        let node = Tree.nodeFromPath(_nodePath, this.state.treeNodes);
+        if (node.childNodes) {
+            let readablePath = getReadablePath(this.state.activeSectionPath);
+            if (readablePath) readablePath += ".";
+            node.childNodes.push(this.getNodeFromQuestionOrSection(section, readablePath + (node.childNodes.length + 1).toString()))
+        }
+        return this.state;
     }
 
     handleChange(section: QuestionSection) {
@@ -147,13 +185,18 @@ export class SurveyForm_ extends React.Component<SurveyFormProps, SurveyFormStat
     handleAddSection() {
         this.setState((prevState: SurveyFormState) => {
             let cloned = _.clone(prevState.form);
+            let toAddIn = SurveyForm_.questionOrSectionFromPath(this.state.activeSectionPath, [cloned]);
+            if (toAddIn instanceof QAQuestion) {
+                return {
+
+                }
+            }
             let newsection = new QuestionSection();
-            cloned.addContent(new QuestionSection());
+            toAddIn.addContent(new QuestionSection());
             this.addSectionToNodeTree(this.state.activeSectionPath, newsection);
             return {
-                form: cloned,
-                treeNodes: this.generateITNodeTree(cloned)
-
+                ...this.state,
+                form: cloned
             }
         }, () => {
             if (this.props.onChange) this.props.onChange(this.state.form);
@@ -163,12 +206,35 @@ export class SurveyForm_ extends React.Component<SurveyFormProps, SurveyFormStat
     handleAddQuestion() {
         this.setState((prevState: SurveyFormState) => {
             let cloned = _.clone(prevState.form);
-            cloned.addContent(new QAQuestion());
-            // let clonedTree = _.clone(prevState.treeNodes);
+            let toAddIn = SurveyForm_.questionOrSectionFromPath(this.state.activeSectionPath, [cloned]);
+            if (toAddIn instanceof QAQuestion) {
+                return {
+
+                }
+            }
+            let q = new QAQuestion();
+            toAddIn.addContent(new QAQuestion());
+
+            this.addQuestionToNodeTree(this.state.activeSectionPath, q)
 
             return {
+                ...this.state,
                 form: cloned,
-                treeNodes: this.generateITNodeTree(cloned)
+            }
+        }, () => {
+            if (this.props.onChange) this.props.onChange(this.state.form);
+        })
+    }
+    handleDeleteQuestionOrSection(deleteid: string) {
+        this.setState((prevState: SurveyFormState) => {
+            let cloned = _.clone(prevState.form);
+            cloned.deleteContent(deleteid);
+
+            this.deleteNodeFromNodeTree(this.state.activeSectionPath, deleteid)
+
+            return {
+                ...this.state,
+                form: cloned,
             }
         }, () => {
             if (this.props.onChange) this.props.onChange(this.state.form);
@@ -224,23 +290,7 @@ export class SurveyForm_ extends React.Component<SurveyFormProps, SurveyFormStat
         this.setState(this.state);
     }
 
-    getItemFromSectionId(id: string) {
-        let sectiondata = id.split(" ")[0];
-        let currentSection: QuestionSection | QAQuestion = this.state.form;
-        if (sectiondata) {
-            let sectionIndices = sectiondata.split(".").map(item => parseInt(item) - 1);
 
-            for (let i = 0; i < sectionIndices.length; i++) {
-                let index = sectionIndices[i];
-
-                if (currentSection instanceof QuestionSection && currentSection.content[index] instanceof QuestionSection) {
-                    currentSection = currentSection.content[i];
-                }
-            }
-        }
-        return currentSection;
-
-    }
     handleFormTreeNodeClick(nodeData: ITreeNode, _nodePath: number[], e: React.MouseEvent<HTMLElement>) {
         console.log(_nodePath);
         const initiallySelected = !!nodeData.isSelected;
@@ -248,26 +298,26 @@ export class SurveyForm_ extends React.Component<SurveyFormProps, SurveyFormStat
             this.forEachNode(this.state.treeNodes, n => (n.isSelected = false));
         }
         nodeData.isSelected = !initiallySelected;
-        let selectedItem = nodeData.label;
+        let item = SurveyForm_.questionOrSectionFromPath(_nodePath, [this.state.form]);
+        console.log(item)
+        if (item instanceof QuestionSection) {
+            this.setState((prevState: SurveyFormState) => {
 
-        if (selectedItem && typeof (selectedItem) === "string") {
-            let item = this.getItemFromSectionId(selectedItem);
-            if (item instanceof QuestionSection) {
-                this.setState((prevState: SurveyFormState) => {
+                return {
+                    ...this.state,
+                    activeSection: item instanceof QuestionSection ? item : prevState.activeSection,
+                    activeSectionPath: _nodePath
+                }
 
-                    return {
-                        ...this.state,
-                        activeSection: item instanceof QuestionSection ? item : prevState.activeSection,
-                    }
-
-                })
-            }
-
+            })
         }
+
+
 
     }
 
     render() {
+
         return (
             <Row>
                 <Toolbar handleItemClick={this.handleToolbarItemClick.bind(this)}></Toolbar>
@@ -279,7 +329,7 @@ export class SurveyForm_ extends React.Component<SurveyFormProps, SurveyFormStat
                         </div>
                     </div>
                     <div className="content">
-                        <SectionC parentPath={""} handleQuestionChange={this.handleQuestionUpdate.bind(this)} section={this.state.activeSection} />
+                        <SectionC handleDeleteChildSectionOrQuestion={this.handleDeleteQuestionOrSection.bind(this)} parentPath={this.state.activeSectionPath} handleQuestionChange={this.handleQuestionUpdate.bind(this)} section={this.state.activeSection} />
                     </div>
                     <Row style={{
                         position: "fixed",
@@ -338,7 +388,8 @@ export class FormTree extends React.Component<FormTreeProps, FormTreeState>{
 interface SectionCProps {
     section: QuestionSection,
     handleQuestionChange: (question: QAQuestion) => void,
-    parentPath: string,
+    parentPath: number[],
+    handleDeleteChildSectionOrQuestion: (deleteid: string) => void,
 }
 interface SectionCState {
 }
@@ -353,16 +404,20 @@ class SectionC extends React.Component<SectionCProps, SectionCState>{
         if (this.props.handleQuestionChange) this.props.handleQuestionChange(q);
     }
 
+
+
     render() {
         let comp = null;
+        let readablePath = getReadablePath(this.props.parentPath);
+        if (readablePath) readablePath += ".";
         comp = this.props.section.content.map((item, index) => {
             if (item instanceof QAQuestion) {
-                return <QuestionButton path={this.props.parentPath + (index + 1)} key={item.id} isExpanded={false}>
+                return <QuestionButton questionId={item.id} handleDeletion={this.props.handleDeleteChildSectionOrQuestion} path={readablePath + (index + 1)} key={item.id} isExpanded={false}>
                     <DPFormItem onChange={this.handleQuestionChange.bind(this)} question={item} />
                 </QuestionButton>
             }
             else if (item instanceof QuestionSection) {
-                return <SectionButton path={this.props.parentPath + (index + 1)} key={item.id} onClick={() => console.log("Sdf")}></SectionButton>
+                return <SectionButton handleDeletion={this.props.handleDeleteChildSectionOrQuestion} sectionId={item.id} path={readablePath + (index + 1)} key={item.id} onClick={() => console.log("Sdf")}></SectionButton>
             }
         })
         return (
@@ -375,17 +430,19 @@ class SectionC extends React.Component<SectionCProps, SectionCState>{
     }
 }
 interface QuestionButtonProps {
+    questionId: string,
     isExpanded: boolean,
     children: ReactNode,
-    path?: string
+    path?: string,
+    handleDeletion: (id: string) => void,
 }
 const QuestionButton = (props: QuestionButtonProps) => {
     const [isExpanded, setIsExpanded] = useState(false);
     return (
         <ButtonGroup className="bp3-dark" style={{ paddingBottom: "20px" }} fill vertical>
             <ButtonGroup>
-                <Button_B style={{ height: 50 }} onClick={() => setIsExpanded(!isExpanded)} alignText={"left"}  rightIcon={isExpanded ? "chevron-up" : "chevron-down"}><Badge color="secondary">Q</Badge> <span>{props.path + " Question"} </span></Button_B>
-                <Button_B style={{ height: 50, width: 20 }} alignText="left"  rightIcon={"cross"} />
+                <Button_B style={{ height: 50 }} onClick={() => setIsExpanded(!isExpanded)} alignText={"left"} rightIcon={isExpanded ? "chevron-up" : "chevron-down"}><Badge color="secondary">Q</Badge> <span>{props.path + " Question"} </span></Button_B>
+                <Button_B onClick={() => props.handleDeletion(props.questionId)} style={{ height: 50, width: 20 }} alignText="left" rightIcon={"cross"} />
             </ButtonGroup>
             <Collapse keepChildrenMounted={false} isOpen={isExpanded}>
                 {props.children}
@@ -395,15 +452,17 @@ const QuestionButton = (props: QuestionButtonProps) => {
 }
 
 interface SectionButtonProps {
+    sectionId: string,
     onClick: (e: React.MouseEvent) => void,
-    path?: string
+    path?: string,
+    handleDeletion: (id: string) => void
 
 }
 const SectionButton = (props: SectionButtonProps) => {
     return (
         <ButtonGroup className="bp3-dark" style={{ paddingBottom: "20px" }} fill >
-            <Button_B style={{ height: 50 }} alignText="left" onClick={props.onClick}  rightIcon={"folder-open"}><Badge color="secondary">S</Badge> <span>{props.path + " Section"} </span></Button_B>
-            <Button_B style={{ height: 50, width: 20 }} alignText="left" onClick={props.onClick} rightIcon={"cross"} />
+            <Button_B style={{ height: 50 }} alignText="left" onClick={props.onClick} rightIcon={"folder-open"}><Badge color="secondary">S</Badge> <span>{props.path + " Section"} </span></Button_B>
+            <Button_B onClick={() => props.handleDeletion(props.sectionId)} style={{ height: 50, width: 20 }} alignText="left"  rightIcon={"cross"} />
 
         </ButtonGroup>
     )
@@ -412,3 +471,7 @@ const SectionButton = (props: SectionButtonProps) => {
 
 
 
+function getReadablePath(nu: number[]) {
+    return nu.slice(1).map(item => item + 1).join(".");
+
+}
