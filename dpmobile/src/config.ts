18caa10d6b4a08bf9b17435ddd4502601766e09e
Anuj Wagle <anujwagle@gmail.com>
2019-11-01 23:32:18 -0230

    Switched to production apollo endpoint

--- dpmobile/src/config.ts
+++ dpmobile/src/config.ts
@@ -48,7 +48,7 @@ const templateAnswers = {
   
 export const APP_CONFIG = {
     serverURL: "http://142.93.151.160:5000/graphql",
-    localServerURL: "http://192.168.2.16:5000/graphql",
+    localServerURL: "http://192.168.2.31:5000/graphql",
     responderValueLocation: "ss-76d550ce-a349-c356-80c6-a48c4fc7f56b[0].q-d9283917-be61-dff8-7580-258fa72bc608",
     templateAnswers: templateAnswers,
 }
