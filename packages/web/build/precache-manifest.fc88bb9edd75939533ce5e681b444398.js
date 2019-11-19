2817c1061a6776450c572aadaf8e5a46a27e1d2d
Anuj Wagle <anujwagle@gmail.com>
2019-11-18 21:46:30 -0330

    adding build folder for publishing demo

---
+++ packages/web/build/precache-manifest.fc88bb9edd75939533ce5e681b444398.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e38735a21bb9fa595abf14910043bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bae58cfd169a2cd2e8c",
+    "url": "/static/css/2.29ddb1ed.chunk.css"
+  },
+  {
+    "revision": "f4ec47f675feab00c761",
+    "url": "/static/css/main.2748f904.chunk.css"
+  },
+  {
+    "revision": "9bae58cfd169a2cd2e8c",
+    "url": "/static/js/2.3fa810a5.chunk.js"
+  },
+  {
+    "revision": "f4ec47f675feab00c761",
+    "url": "/static/js/main.1b77f895.chunk.js"
+  },
+  {
+    "revision": "6f5d290041a5f4beef4b",
+    "url": "/static/js/runtime-main.8424871c.js"
+  },
+  {
+    "revision": "782154b83b6b419269980b375144b1f7",
+    "url": "/static/media/icons-16.782154b8.ttf"
+  },
+  {
+    "revision": "7fb844a70d4ff3aa49441242b4149777",
+    "url": "/static/media/icons-16.7fb844a7.woff"
+  },
+  {
+    "revision": "d4f6722b1ac527dd937155d419ec8ddd",
+    "url": "/static/media/icons-16.d4f6722b.eot"
+  },
+  {
+    "revision": "90afdd237a469c44c9a1a3b0ff2db197",
+    "url": "/static/media/icons-20.90afdd23.eot"
+  },
+  {
+    "revision": "c0e4874e23b8c9100e566ca2ca287128",
+    "url": "/static/media/icons-20.c0e4874e.woff"
+  },
+  {
+    "revision": "d1f0f6c1a1c239a03914f7bd06ccfe68",
+    "url": "/static/media/icons-20.d1f0f6c1.ttf"
+  }
+]);
