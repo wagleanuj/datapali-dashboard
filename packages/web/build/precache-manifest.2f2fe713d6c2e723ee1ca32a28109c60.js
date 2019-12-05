5ac4a7b13ec4f523d0f49eb2e4c4ef6161203e68
Anuj Wagle <anujwagle@gmail.com>
2019-12-04 22:07:24 -0330

    build

---
+++ packages/web/build/precache-manifest.2f2fe713d6c2e723ee1ca32a28109c60.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4623111263669eb07f152924dc399f55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b054610a122e249503c6",
+    "url": "/static/css/2.67b4ee9a.chunk.css"
+  },
+  {
+    "revision": "0793a41cc72ae91b2e8a",
+    "url": "/static/css/main.1833a965.chunk.css"
+  },
+  {
+    "revision": "b054610a122e249503c6",
+    "url": "/static/js/2.b4b62edd.chunk.js"
+  },
+  {
+    "revision": "0793a41cc72ae91b2e8a",
+    "url": "/static/js/main.3a581a34.chunk.js"
+  },
+  {
+    "revision": "6f5d290041a5f4beef4b",
+    "url": "/static/js/runtime-main.8424871c.js"
+  },
+  {
+    "revision": "782154b83b6b419269980b375144b1f7",
+    "url": "/static/media/icons-16.782154b8.ttf"
+  },
+  {
+    "revision": "7fb844a70d4ff3aa49441242b4149777",
+    "url": "/static/media/icons-16.7fb844a7.woff"
+  },
+  {
+    "revision": "d4f6722b1ac527dd937155d419ec8ddd",
+    "url": "/static/media/icons-16.d4f6722b.eot"
+  },
+  {
+    "revision": "90afdd237a469c44c9a1a3b0ff2db197",
+    "url": "/static/media/icons-20.90afdd23.eot"
+  },
+  {
+    "revision": "c0e4874e23b8c9100e566ca2ca287128",
+    "url": "/static/media/icons-20.c0e4874e.woff"
+  },
+  {
+    "revision": "d1f0f6c1a1c239a03914f7bd06ccfe68",
+    "url": "/static/media/icons-20.d1f0f6c1.ttf"
+  }
+]);
